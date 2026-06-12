d83a599062ba15c835a6317b2940a4423072a74a
AngelPoot <angelpootucan7@gmail.com>
2026-06-11 22:55:33 -0600

    refactor: update docker-compose and app configuration for environment variables - replaced hardcoded database credentials in docker-compose.yml with environment variables - integrated ConfigModule in app.module.ts for global environment variable management and validation - modified main.ts to utilize ConfigService for dynamic port and environment settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,35 @@ import { AppService } from './app.service';
 import { PrismaModule } from './shared/prisma.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { validationSchema } from './config/validation.schema';
 
 /**
  * Módulo raíz de la aplicación.
  * Registra los slices principales: user y auth.
  */
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule],
+  imports: [
+    // Carga el archivo de configuración .env en el objeto global process.env
+    ConfigModule.forRoot({
+      // Validamos que el uso de las variables de entorno sean de manera global
+      isGlobal: true, 
+      // Ruta del archivo .env
+      envFilePath: '.env',
+      // Esquema de validación
+      validationSchema: validationSchema,
+      // Opciones de validación
+      validationOptions: {
+        allowUnknown: true, // Permite variables de entorno no definidas
+        abortEarly: true, // Detiene la validación al primer error encontrado
+      }
+    }),
+
+    // TypeOrmModule.
+    PrismaModule, 
+    UserModule, 
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
